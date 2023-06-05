56160c20e828df64557e2b0c84979f7bc422b72b
Carlos Cueto <crcueto25@gmail.com>
2023-06-05 14:26:32 +0300

    update steps
    update numbers
    comment swig part (build)

--- installation.sh
+++ installation.sh
@@ -8,27 +8,38 @@ pip3 install cflib transforms3d
 sudo apt-get install ros-galactic-tf-transformations
 
 
-4.2 Install swig and build python bindings
+3 Install swig and build python bindings
 
-4.2.1 Install prerequisites for swig
+3.1 Install prerequisites for swig
 sudo apt install build-essential libpcre2-dev libpcre3-dev autoconf automake libtool bison git libboost-dev golang-go guile-2.2-dev nodejs node-gyp libwebkit2gtk-4.0-dev lua5.3 liblua5.3-dev mono-devel octave liboctave-dev default-jdk-headless php-cli php-dev python-dev python3-dev r-base ruby ruby-dev tcl-dev scilab libxml2-dev
+
 
 sudo apt-get install make gcc-arm-none-eabi
 
+sudo apt -y install swig
 
-4.2.2 Build swig
-cd
-git clone https://github.com/swig/swig.git  -> in home directory
-cd ~/swig
-./autogen.sh && ./configure && make
+3.2 Install pycffirmware
 
-make -k check-test-suite
-make check-python-test-suite
-sudo make install
+pip3 install numpy
+sudo apt install build-essential
 
 
+git clone https://github.com/utiasDSL/pycffirmware.git --recursive
+cd ~/pycffirmware/wrapper
+chmod +x build_linux.sh
+./build_linux.sh
 
-3. Setup ROS 2 workspace
+# 4.2.2 Build swig
+# cd
+# git clone https://github.com/swig/swig.git  -> in home directory
+# cd ~/swig
+# ./autogen.sh && ./configure && make
+
+# make -k check-test-suite
+# make check-python-test-suite
+# sudo make install
+
+4. Setup ROS 2 workspace
 mkdir -p ~/crazyflie-ws/src
 cd ~/crazyflie-ws/src
 git clone https://github.com/IMRCLab/crazyswarm2 --recursive
@@ -38,7 +49,7 @@ colcon build --symlink-install
 
 
 
-4.1 Build Crazyflie firmware
+5 Build Crazyflie firmware
 
 git clone --recursive https://github.com/bitcraze/crazyflie-firmware.git -> in home directory
 cd ~/crazyflie-firmware
@@ -54,12 +65,12 @@ make bindings_python
 
 make unit
 
-5. Export builds to path
+6. Export builds to path
 export PYTHONPATH=~/crazyflie-firmware/build:$PYTHONPATH
 
-5. Run example
+7. Run example
 source ~/crazyflies_ws/install/setup.bash
 ros2 launch crazyflie_examples keyboard_velmux_launch.py
-
+ros2 launch crazyflie launch.py backend:=sim
 
 
